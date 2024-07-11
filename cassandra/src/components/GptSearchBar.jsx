bc293ca3f962b3190517f560e942cde2e455ef4b
Hritvik Agarwal <agarwal.hritvik18@gmail.com>
2024-07-11 20:41:21 +0530

    GptSearchBar need to add the GPT API and the openAi library

--- cassandra/src/components/GptSearchBar.jsx
+++ cassandra/src/components/GptSearchBar.jsx
@@ -1,17 +1,29 @@
 import { useSelector } from "react-redux";
 import { lang } from "../utils/constants";
+import { useRef } from "react";
 
 const GptSearchBar = () => {
   const langKey = useSelector((store) => store.config.lang);
+  const searchText = useRef(null);
+  const handleGptSearch = () => {
+    console.log(searchText.current.value);
+  };
   return (
     <div className="absolute z-30 mt-[15%] w-full flex justify-center">
-      <form className="w-[45rem] bg-black grid grid-cols-12">
+      <form
+        className="w-[45rem] bg-black grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
+          ref={searchText}
           type="text"
           placeholder={lang[langKey].gptSearchPlaceholder}
           className="p-4 m-4 col-span-9"
         ></input>
-        <button className="col-span-3 bg-red-700 text-white rounded-lg m-4 py-2 px-4">
+        <button
+          className="col-span-3 bg-red-700 text-white rounded-lg m-4 py-2 px-4"
+          onClick={handleGptSearch}
+        >
           {lang[langKey].search}
         </button>
       </form>
